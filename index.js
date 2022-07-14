a859047f0eda8c4daea0dc9e97ebb0839438f5cd
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-07-14 11:17:24 +0530

    Add get api examples hands on

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const express = require("express");
-const app = express()
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/public"));
 
-app.listen(8080)
+app.get("/", function(request, response){
+    response.send("hello!");
+});
+
+app.get("/date", (request, response) => {
+    let date = new Date();
+    response.send(`Today's date is ${date}`);
+  });
+
+app.listen(8080, 10.21);
+console.log("Listening on port" + 8080);
